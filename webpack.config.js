7a47947d0d89f4d0d3fce72d20044cb4bc0dc264
Yuriy Korpachan <korpachan@gmail.com>
2016-10-05 08:01:42 +0300

    webpack + tsc

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+﻿const libraryName = "tenogy",
+	p = process.argv.indexOf("-p") !== -1;
+
+module.exports = {
+	entry: [
+		__dirname + "/src/tenogy.ts"
+	],
+
+	//devtool: "source-map",
+
+	resolve: {
+		extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+	},
+
+	module: {
+		loaders: [
+			{ test: /\.json$/, loader: "json-loader" },
+			{ test: /\.tsx?$/, loader: "ts-loader?silent=true" }
+		]
+	},
+
+	output: {
+		path: __dirname + "/dist",
+		filename: libraryName + (p ? ".min.js" : ".js"),
+		library: libraryName,
+		libraryTarget: "umd",
+		umdNamedDefine: true
+	}
+};
