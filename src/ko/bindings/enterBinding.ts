7911ae4ddd771ec977b368ee046ef6271e14d1bf
Yuriy Korpachan <korpachan@gmail.com>
2016-12-08 23:44:07 +0300

    fix enter binding

--- src/ko/bindings/enterBinding.ts
+++ src/ko/bindings/enterBinding.ts
@@ -4,9 +4,8 @@ export class EnterBinding extends BaseBinding {
 	init(element: HTMLElement, valueAccessor: () => any, allBindingsAccessor?: KnockoutAllBindingsAccessor, viewModel?: any, bindingContext?: KnockoutBindingContext) {
 		$(element).keypress((event: any) => {
 			var code = event.keyCode || event.which;
-			if (code != 13) return;
+			if (code !== 13) return;
 			var handler = super.unwrap(valueAccessor());
-			$(element).blur();
 			handler.call(viewModel);
 			event.preventDefault();
 		});
