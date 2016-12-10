c3dcbe38dbf69a64fa08fcaf06e98282ec6153ea
Yuriy Korpachan <korpachan@gmail.com>
2016-12-10 11:23:32 +0300

    add bootstrap modal

---
+++ src/ko/bindings/bootstrapModalBinding.ts
@@ -0,0 +1,80 @@
+﻿import { BaseBinding } from "./../core/baseBinding";
+import * as u from "./../../utils";
+
+export class BootstrapModal {
+	shown: KnockoutObservable<boolean>;
+
+	constructor() {
+		this.shown = ko.observable(false);
+	}
+
+	show(model) {
+		if (!model.$modal) {
+			model.$modal = { close: () => this.close() }
+		}
+		this.shown(model as boolean);
+	}
+
+	close() {
+		this.shown(false);
+	}
+};
+
+export class BootstrapModalBinding extends BaseBinding {
+	init(element: HTMLElement, valueAccessor: () => any, allBindingsAccessor?: KnockoutAllBindingsAccessor, viewModel?: any, bindingContext?: KnockoutBindingContext) {
+		var $element:any = $(element);
+		var config = super.unwrap(valueAccessor());
+		var controller = config.controller;
+		var options:any = { show: false, backdrop: false, keyboard: false };
+		if (controller) {
+		
+			options = { ...config.options, ...options};
+		}
+		else {
+			controller = config;
+		}
+
+		if (options.zindex) {
+			$element.css("z-index", parseInt(options.zindex) + 1);
+		}
+
+		if (config.html) {
+			$element.html(config.html);
+		}
+		$element.modal(options);
+
+		var context = {
+			isBound: false,
+			viewModel: ko.observable(null)
+		};
+
+		controller.shown.subscribe(value => {
+			$element.modal(value ? "show" : "hide");
+			if (value) {
+				if (options.zindex) {
+					$(".modal-backdrop").last().css("z-index", options.zindex);
+				}
+
+				context.viewModel(value);
+				if (!context.isBound) {
+					var bindingContextExtension = {
+						$modal: {
+							close() { controller.shown(null); }
+						}
+					};
+					var bindingContext = new (ko as any).bindingContext(context.viewModel, null, null, ctx => { ko.utils.extend(ctx, bindingContextExtension); });
+					ko.applyBindingsToDescendants(bindingContext, element);
+					context.isBound = true;
+				}
+			}
+		});
+
+		$(element).on('hidden.bs.modal', () => {
+			controller.shown(null);
+		});
+
+		return { controlsDescendantBindings: true };
+	}
+}
+
+BaseBinding.register("bootstrapModal", BootstrapModalBinding, true);
--- src/tenogy.ts
+++ src/tenogy.ts
@@ -13,6 +13,7 @@ export * from "./ko/bindings/summerNoteBinding";
 export * from "./ko/bindings/fileUploadBinding";
 export * from "./ko/bindings/htmlStatefulBinding";
 export * from "./ko/bindings/select2Binding";
+export * from "./ko/bindings/bootstrapModalBinding";
 
 export * from "./ko/alert";
 export * from "./ko/loadingProgress";
